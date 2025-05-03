05b9f347875a90c0bbd27c0a6439244fa2a709f0
Paulo Pertierra <paulopertierra@gmail.com>
2025-05-04 00:19:42 +0800

    feat: add homeview delete and add notes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,46 @@
 <script setup lang="ts">
+import { onMounted, reactive, ref, type Ref } from 'vue';
 import TheWelcome from '../components/TheWelcome.vue'
+import type { Note } from '@/types/note';
+
+const formData = reactive({
+  title: "",
+  description: "",
+  content: ""
+});
 
 async function submitForm() {
   console.log("Note submitted.");
-} 
+
+  await fetch("/api/notes", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(formData)
+  });
+
+  const response = await fetch("/api/notes");
+  notes.value = (await response.json()).data
+}
+
+const notes: Ref<Note[]> = ref([]);
+
+onMounted(async () => {
+  console.log("Te")
+  const response = await fetch("/api/notes");
+  notes.value = (await response.json()).data
+});
+
+async function deleteNote(id: number) {
+
+  const deleteResponse = await fetch(`/api/notes/${id}`, {
+    method: "DELETE"
+  });
+
+  const response = await fetch("/api/notes");
+  notes.value = (await response.json()).data
+}
 </script>
 
 <template>
@@ -12,21 +49,24 @@ async function submitForm() {
     <table>
       <colgroup>
         <col style="width: 20%;">
-        <col style="width: 30%;">
-        <col style="width: 50%;">
+        <col style="width: 20%;">
+        <col style="width: 40%;">
+        <col style="width: 10%;">
       </colgroup>
       <thead>
         <tr>
           <th>Name</th>
           <th>Note Description</th>
           <th>Content</th>
+          <th>Action</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td>Example Note</td>
-          <td>A short description</td>
-          <td>This is the content of the note. It has more space to hold more text.</td>
+        <tr v-for="note in notes">
+          <td>{{ note.title }}</td>
+          <td>{{ note.description }}</td>
+          <td>{{ note.content }}</td>
+          <td><button @click="deleteNote(note.id)">Delete</button></td>
         </tr>
       </tbody>
     </table>
@@ -34,17 +74,17 @@ async function submitForm() {
     <form @submit.prevent="submitForm()">
       <label>
         Name:<br>
-        <input required type="text" name="name">
+        <input v-model="formData.title" required type="text" name="name">
       </label>
       <br><br>
       <label>
         Note Description:<br>
-        <input type="text" name="description">
+        <input v-model="formData.description" type="text" name="description">
       </label>
       <br><br>
       <label>
         Content:<br>
-        <textarea required name="content" rows="4" cols="50"></textarea>
+        <textarea v-model="formData.content" required name="content" rows="4" cols="50"></textarea>
       </label>
       <br><br>
       <button type="submit">Add Note</button>
